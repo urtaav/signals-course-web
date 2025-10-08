8a5b8b4512317fd5cb5507afb18deb8f25ba9f6f
urtaav <urieltapiaavalos@gmail.com>
2025-10-07 23:00:26 -0600

    feat: rxResource and TakeUntilDestroyed pages

--- src/app/modules/rxjs-interop/pages/interop-rx-resource/interop-rx-resource.ts
+++ src/app/modules/rxjs-interop/pages/interop-rx-resource/interop-rx-resource.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import * as fromComponents from '../../../../shared/components';
+import { HttpClient } from '@angular/common/http';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+interface Post { id: number; title: string; }
+
 
 @Component({
   selector: 'app-interop-rx-resource',
-  imports: [],
+  imports: [...fromComponents.components],
   templateUrl: './interop-rx-resource.html',
-  styleUrl: './interop-rx-resource.css'
+  styleUrl: './interop-rx-resource.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InteropRxResource {
+  readonly http: HttpClient = inject(HttpClient);
 
+  readonly posts = rxResource<Post[],void>({
+    stream: () => this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts'),
+    defaultValue:[]
+  });
 }
--- src/app/modules/rxjs-interop/pages/interop-rx-take-until-destroyed/interop-rx-take-until-destroyed.ts
+++ src/app/modules/rxjs-interop/pages/interop-rx-take-until-destroyed/interop-rx-take-until-destroyed.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
-
+import * as fromComponents from '../../../../shared/components';
+import { interval, map } from 'rxjs';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-interop-rx-take-until-destroyed',
-  imports: [],
+  imports: [...fromComponents.components, AsyncPipe],
   templateUrl: './interop-rx-take-until-destroyed.html',
   styleUrl: './interop-rx-take-until-destroyed.css'
 })
 export class InteropRxTakeUntilDestroyed {
+  private ticker$ = interval(1000);
 
+  //Hace que el observable se detenga automáticamente cuando el componente se destruye.
+  readonly counter = this.ticker$.pipe(takeUntilDestroyed());
+  readonly counterSignal = toSignal(this.ticker$.pipe(takeUntilDestroyed()));
+
+  // Emite cada segundo y devuelve la hora formateada
+ private timer$ = interval(1000).pipe(
+    map(() => new Date().toLocaleTimeString()),
+    takeUntilDestroyed()
+  );
+
+  // Convierte el observable a signal reactivo (Angular 16+)
+  readonly time = toSignal(this.timer$, { initialValue: new Date().toLocaleTimeString() });
+
+//   Qué hace:
+
+// interval(1000) → emite cada segundo.
+
+// map(...) → transforma la emisión en una cadena de texto con la hora actual.
+
+// takeUntilDestroyed() → detiene el stream automáticamente cuando el componente se destruye (sin fugas).
+
+// toSignal() → convierte el observable en un Signal que puedes usar directamente en el template como una función reactiva ({{ time() }}).
+  
+ ngOnDestroy() {
+    console.log('💥 ngOnDestroy Component');
+  }
 }
